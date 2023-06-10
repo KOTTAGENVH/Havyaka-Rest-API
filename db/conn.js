e98f210e52e7622f7d88871e3fd49646b7dca309
gaonkarBhai <naveengaonkar014@gmail.com>
2023-06-10 19:25:22 +0530

    testing

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose")
 
-// kC6e4BZOq01XMzfs naveen
-// mongodb+srv://naveen:kC6e4BZOq01XMzfs@cluster0.mpu2rc1.mongodb.net/mantraapiretryWrites=true&w=majority
-
-
-
 const connDB = (URI) => {
     console.log(">> DB Connected");
     return mongoose.connect(URI);
